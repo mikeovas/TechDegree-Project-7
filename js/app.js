ef8d53d5c44a20c22d6d6cf175edcb31606c6455
mikeovas <mikeovas@gmail.com>
2022-01-04 17:53:45 -0500

    removed temp sections

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ alertBanner.innerHTML = `<div class = "alert-banner">
                         <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
                         <p class = "alert-banner-close">x</p>
                         </div>`
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+});
+
+const trafficCanvas = document.getElementById("traffic-chart");
